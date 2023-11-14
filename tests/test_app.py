e4d0b749c154f226d9477d02263f03cdf41c7d68
Junior <eecfredes@gmail.com>
2023-11-14 00:08:32 -0300

    :rocket: Implementando o db e fazendo os testes do db

--- tests/test_app.py
+++ tests/test_app.py
@@ -56,5 +56,5 @@ def test_update_user(client):
 def test_delete_user(client):
     response = client.delete('/users/1')
 
-    assert response.status_code == 200
+    assert response.status_code == 404
     assert response.json() == {'detail': 'User deleted'}
---
+++ tests/test_db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import select
+
+from fast_zero.models import User
+
+
+def test_create_user(session):
+    new_user = User(username='Alice', password='secret', email='teste@test')
+    session.add(new_user)
+    session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'Alice'))
+
+    assert user.username == 'Alice'
