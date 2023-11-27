4e7db928721df18ba3b4bc2c0655aebdce7e4fc9
Junior <eecfredes@gmail.com>
2023-11-27 00:04:36 -0300

    :rocket: Modificando a rota Delete

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -8,39 +8,31 @@ from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
-database = []
 
-
-@app.get('/', status_code=status.HTTP_200_OK)
+@app.get("/", status_code=status.HTTP_200_OK)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {"message": "Olá Mundo!"}
 
 
-@app.get('/users/', status_code=status.HTTP_200_OK, response_model=UserList)
+@app.get("/users/", status_code=status.HTTP_200_OK, response_model=UserList)
 def read_users(
     skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
 ):
     users = session.scalars(select(User).offset(skip).limit(limit)).all()
-    return {'users': users, 'count': len(users)}
+    return {"users": users, "count": len(users)}
 
 
-@app.post(
-    '/users/', status_code=status.HTTP_201_CREATED, response_model=UserPublic
-)
+@app.post("/users/", status_code=status.HTTP_201_CREATED, response_model=UserPublic)
 def create_user(user: UserSchema, session: Session = Depends(get_session)):
-    db_user = session.scalar(
-        select(User).where(User.username == user.username)
-    )
+    db_user = session.scalar(select(User).where(User.username == user.username))
 
     if db_user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Username already exists',
+            detail="Username already exists",
         )
 
-    db_user = User(
-        username=user.username, password=user.password, email=user.email
-    )
+    db_user = User(username=user.username, password=user.password, email=user.email)
     session.add(db_user)
     session.commit()
     session.refresh(db_user)
@@ -49,7 +41,7 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
 
 @app.put(
-    '/users/{user_id}',
+    "/users/{user_id}",
     status_code=status.HTTP_200_OK,
     response_model=UserPublic,
 )
@@ -58,7 +50,9 @@ def update_user(
 ):
     db_user = session.scalar(select(User).where(User.id == user_id))
     if db_user is None:
-        raise HTTPException(status_code=404, detail='User not found')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
 
     db_user.username = user.username
     db_user.password = user.password
@@ -70,14 +64,19 @@ def update_user(
 
 
 @app.delete(
-    '/users/{user_id}',
+    "/users/{user_id}",
     status_code=status.HTTP_404_NOT_FOUND,
     response_model=Message,
 )
-def delete_user(user_id: int):
-    if user_id > len(database) or user_id < 1:
-        raise HTTPException(status_code=404, detail='User not found')
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
 
-    del database[user_id - 1]
+    if db_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
 
-    return {'detail': 'User deleted'}
+    session.delete(db_user)
+    session.commit()
+
+    return {"detail": "User deleted"}
