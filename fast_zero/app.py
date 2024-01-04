57eb4559bfde7e9e25868ea9ec1ae8c518027b7f
Junior <eecfredes@gmail.com>
2024-01-03 21:34:34 -0300

    :rocket: Aplicando as protecoes aos endpoints

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -8,6 +8,7 @@ from fast_zero.models import User
 from fast_zero.schemas import Message, Token, UserList, UserPublic, UserSchema
 from fast_zero.security import (
     create_access_token,
+    get_current_user,
     get_password_hash,
     verify_password,
 )
@@ -15,7 +16,7 @@ from fast_zero.security import (
 app = FastAPI()
 
 
-@app.post('/token', response_model=Token)
+@app.post("/token", response_model=Token)
 def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
     session: Session = Depends(get_session),
@@ -25,44 +26,40 @@ def login_for_access_token(
     if not user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Incorrect email or password',
+            detail="Incorrect email or password",
         )
 
     if not verify_password(form_data.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Incorrect email or password',
+            detail="Incorrect email or password",
         )
 
-    access_token = create_access_token(data={'sub': user.email})
-    return {'access_token': access_token, 'token_type': 'bearer'}
+    access_token = create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
-@app.get('/', status_code=status.HTTP_200_OK)
+@app.get("/", status_code=status.HTTP_200_OK)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {"message": "Olá Mundo!"}
 
 
-@app.get('/users/', status_code=status.HTTP_200_OK, response_model=UserList)
+@app.get("/users/", status_code=status.HTTP_200_OK, response_model=UserList)
 def read_users(
     skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
 ):
     users = session.scalars(select(User).offset(skip).limit(limit)).all()
-    return {'users': users, 'count': len(users)}
+    return {"users": users, "count": len(users)}
 
 
-@app.post(
-    '/users/', status_code=status.HTTP_201_CREATED, response_model=UserPublic
-)
+@app.post("/users/", status_code=status.HTTP_201_CREATED, response_model=UserPublic)
 def create_user(user: UserSchema, session: Session = Depends(get_session)):
-    db_user = session.scalar(
-        select(User).where(User.username == user.username)
-    )
+    db_user = session.scalar(select(User).where(User.username == user.username))
 
     if db_user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Username already exists',
+            detail="Username already exists",
         )
 
     hashed_password = get_password_hash(user.password)
@@ -79,41 +76,58 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
 
 @app.put(
-    '/users/{user_id}',
+    "/users/{user_id}",
     status_code=status.HTTP_200_OK,
     response_model=UserPublic,
 )
 def update_user(
-    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
 ):
-    db_user = session.scalar(select(User).where(User.id == user_id))
-    if db_user is None:
+    if current_user.id != user_id:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail='User not found'
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Not enough permissions"
         )
 
-    db_user.username = user.username
-    db_user.password = get_password_hash(user.password)
-    db_user.email = user.email
+    # db_user = session.scalar(select(User).where(User.id == user_id))
+    # if db_user is None:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+    #     )
+
+    current_user.username = user.username
+    current_user.password = user.password
+    current_user.email = user.email
     session.commit()
-    session.refresh(db_user)
-    return db_user
+    session.refresh(current_user)
+    return current_user
 
 
 @app.delete(
-    '/users/{user_id}',
+    "/users/{user_id}",
     status_code=status.HTTP_404_NOT_FOUND,
     response_model=Message,
 )
-def delete_user(user_id: int, session: Session = Depends(get_session)):
-    db_user = session.scalar(select(User).where(User.id == user_id))
-
-    if db_user is None:
+def delete_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
+    if current_user.id != user_id:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail='User not found'
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Not enough permissions"
         )
 
-    session.delete(db_user)
+    # db_user = session.scalar(select(User).where(User.id == user_id))
+
+    # if db_user is None:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+    #     )
+
+    session.delete(current_user)
     session.commit()
 
-    return {'detail': 'User deleted'}
+    return {"detail": "User deleted"}
