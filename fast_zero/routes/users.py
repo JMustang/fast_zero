631afa61fd65a720922678626ea6de832444237b
Junior <eecfredes@gmail.com>
2024-01-05 21:12:09 -0300

    :recycle: Refatorando as rotas, de testes

--- fast_zero/routes/users.py
+++ fast_zero/routes/users.py
@@ -58,15 +58,9 @@ def update_user(
 ):
     if current_user.id != user_id:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Not enough permissions',
         )
-
-    # db_user = session.scalar(select(User).where(User.id == user_id))
-    # if db_user is None:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-    #     )
 
     current_user.username = user.username
     current_user.password = user.password
