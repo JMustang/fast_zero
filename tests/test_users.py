013590f35fa7237f54c09110a5cfe4878285a329
Junior <eecfredes@gmail.com>
2024-01-07 16:51:31 -0300

    :white_check_mark: Testando erro de usuario errado na atualizacao

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,56 +3,70 @@ from fast_zero.schemas import UserPublic
 
 def test_create_user(client):
     response = client.post(
-        '/users/',
+        "/users/",
         json={
-            'username': 'alice',
-            'email': 'alice@example.com',
-            'password': 'secret',
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "secret",
         },
     )
     assert response.status_code == 201
     assert response.json() == {
-        'username': 'alice',
-        'email': 'alice@example.com',
-        'id': 1,
+        "username": "alice",
+        "email": "alice@example.com",
+        "id": 1,
     }
 
 
 def test_read_users(client):
-    response = client.get('/users/')
+    response = client.get("/users/")
     assert response.status_code == 200
-    assert response.json() == {'users': []}
+    assert response.json() == {"users": []}
 
 
 def test_read_users_with_users(client, user):
     user_schema = UserPublic.model_validate(user).model_dump()
-    response = client.get('/users/')
-    assert response.json() == {'users': [user_schema]}
+    response = client.get("/users/")
+    assert response.json() == {"users": [user_schema]}
 
 
 def test_update_user(client, user, token):
     response = client.put(
-        f'/users/{user.id}',
-        headers={'Authorization': f'Bearer {token}'},
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
         json={
-            'username': 'bob',
-            'email': 'bob@example.com',
-            'password': 'mynewpassword',
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
         },
     )
     assert response.status_code == 200
     assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@example.com',
-        'id': 1,
+        "username": "bob",
+        "email": "bob@example.com",
+        "id": 1,
     }
+
+
+def test_update_user_with_wrong_user(client, user, token):
+    response = client.put(
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+        json={
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Not enough permissions!"}
 
 
 def test_delete_user(client, user, token):
     response = client.delete(
-        f'/users/{user.id}',
-        headers={'Authorization': f'Bearer {token}'},
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
     )
 
     assert response.status_code == 404
-    assert response.json() == {'detail': 'User deleted'}
+    assert response.json() == {"detail": "User deleted"}
