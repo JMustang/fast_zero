540393ad63501cc1e3988b1b3c73098f91a10cbd
Junior <eecfredes@gmail.com>
2024-01-07 00:39:52 -0300

    :rocket: Usando Annotated da lib typing

--- fast_zero/routes/users.py
+++ fast_zero/routes/users.py
@@ -1,35 +1,33 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+from typing import Annotated
 
 from fast_zero.database import get_session
 from fast_zero.models import User
 from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
 from fast_zero.security import get_current_user, get_password_hash
 
-router = APIRouter(prefix='/users', tags=['Users'])
+router = APIRouter(prefix="/users", tags=["Users"])
+
+Session = Annotated[Session, Depends(get_session)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
-@router.get('/', status_code=status.HTTP_200_OK, response_model=UserList)
-def read_users(
-    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
-):
+@router.get("/", status_code=status.HTTP_200_OK, response_model=UserList)
+def read_users(skip: int = 0, limit: int = 100, session: Session = Session):
     users = session.scalars(select(User).offset(skip).limit(limit)).all()
-    return {'users': users, 'count': len(users)}
+    return {"users": users, "count": len(users)}
 
 
-@router.post(
-    '/', status_code=status.HTTP_201_CREATED, response_model=UserPublic
-)
-def create_user(user: UserSchema, session: Session = Depends(get_session)):
-    db_user = session.scalar(
-        select(User).where(User.username == user.username)
-    )
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserPublic)
+def create_user(user: UserSchema, session: Session = Session):
+    db_user = session.scalar(select(User).where(User.username == user.username))
 
     if db_user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Username already exists',
+            detail="Username already exists",
         )
 
     hashed_password = get_password_hash(user.password)
@@ -46,20 +44,20 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
 
 @router.put(
-    '/{user_id}',
+    "/{user_id}",
     status_code=status.HTTP_200_OK,
     response_model=UserPublic,
 )
 def update_user(
     user_id: int,
     user: UserSchema,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: Session,
+    current_user: CurrentUser,
 ):
     if current_user.id != user_id:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Not enough permissions',
+            detail="Not enough permissions",
         )
 
     current_user.username = user.username
@@ -71,19 +69,19 @@ def update_user(
 
 
 @router.delete(
-    '/{user_id}',
+    "/{user_id}",
     status_code=status.HTTP_404_NOT_FOUND,
     response_model=Message,
 )
 def delete_user(
     user_id: int,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: Session,
+    current_user: CurrentUser,
 ):
     if current_user.id != user_id:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Not enough permissions',
+            detail="Not enough permissions",
         )
 
     # db_user = session.scalar(select(User).where(User.id == user_id))
@@ -96,4 +94,4 @@ def delete_user(
     session.delete(current_user)
     session.commit()
 
-    return {'detail': 'User deleted'}
+    return {"detail": "User deleted"}
