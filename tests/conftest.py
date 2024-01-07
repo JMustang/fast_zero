fc8086fb2c4c1223e4596041c3ed9cb8881887d2
Junior <eecfredes@gmail.com>
2024-01-07 17:07:46 -0300

    :recycle: Usando a factory-boy lib para criar fake users de testes

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
+import factory
 
 from fast_zero.app import app
 from fast_zero.database import get_session
@@ -10,11 +11,21 @@ from fast_zero.models import Base, User
 from fast_zero.security import get_password_hash
 
 
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    id = factory.Sequence(lambda n: n)
+    username = factory.LazyAttribute(lambda obj: f"test{obj.id}")
+    email = factory.LazyAttribute(lambda obj: f"{obj.username}@test.com")
+    password = factory.LazyAttribute(lambda obj: f"{obj.username}@example.com")
+
+
 @pytest.fixture
 def session():
     engine = create_engine(
-        'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},
+        "sqlite:///:memory:",
+        connect_args={"check_same_thread": False},
         poolclass=StaticPool,
     )
     Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -37,17 +48,28 @@ def client(session):
 
 @pytest.fixture
 def user(session):
-    password = 'testtest'
-    user = User(
-        username='Teste',
-        email='teste@test.com',
-        password=get_password_hash(password),
-    )
+    password = "testtest"
+    user = UserFactory(password=get_password_hash(password))
+
     session.add(user)
     session.commit()
     session.refresh(user)
 
-    user.clean_password = 'testtest'
+    user.clean_password = "testtest"
+
+    return user
+
+
+@pytest.fixture
+def other_user(session):
+    password = "testtest"
+    user = UserFactory(password=get_password_hash(password))
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    user.clean_password = "testtest"
 
     return user
 
@@ -55,7 +77,7 @@ def user(session):
 @pytest.fixture
 def token(client, user):
     response = client.post(
-        '/auth/token',
-        data={'username': user.email, 'password': user.clean_password},
+        "/auth/token",
+        data={"username": user.email, "password": user.clean_password},
     )
-    return response.json()['access_token']
+    return response.json()["access_token"]
