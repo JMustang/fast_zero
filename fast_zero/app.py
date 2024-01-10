ab2ce355ff591d95dcf9b979c57ccde3cef3b9d8
Junior <eecfredes@gmail.com>
2024-01-10 00:03:00 -0300

    :rocket: Criando uma nova rota todos

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, status
 
-from fast_zero.routes import auth, users
+from fast_zero.routes import auth, users, todos
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todos.router)
 
 
-@app.get('/', status_code=status.HTTP_200_OK)
+@app.get("/", status_code=status.HTTP_200_OK)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {"message": "Olá Mundo!"}
