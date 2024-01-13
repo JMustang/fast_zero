5de6984b53de77acb525e8de22b4cfa280332b65
Junior <eecfredes@gmail.com>
2024-01-12 22:07:25 -0300

    :rocket: Criando uma rota POST, create_todo

--- fast_zero/routes/todos.py
+++ fast_zero/routes/todos.py
@@ -1,3 +1,32 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-router = APIRouter(prefix='/todos', tags=['todos'])
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from fast_zero.database import get_session
+from fast_zero.models import Todo, User
+from fast_zero.schemas import TodoPublic, TodoSchema
+from fast_zero.security import get_current_user
+
+router = APIRouter()
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
+router = APIRouter(prefix="/todos", tags=["todos"])
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=TodoPublic)
+def create_todo(
+    todo: TodoSchema, user: CurrentUser, session: Session = Depends(get_session)
+):
+    db_todo: Todo = Todo(
+        title=todo.title,
+        description=todo.description,
+        state=todo.state,
+        user_id=user.id,
+    )
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+
+    return db_todo
