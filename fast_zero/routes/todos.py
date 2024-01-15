6c23d750451ba9ac63620627ffc1d067838cacd3
Junior <eecfredes@gmail.com>
2024-01-14 21:22:29 -0300

    :rocket: Criando uma rota GET, list_todos

--- fast_zero/routes/todos.py
+++ fast_zero/routes/todos.py
@@ -1,23 +1,22 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Query
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import Todo, User
-from fast_zero.schemas import TodoPublic, TodoSchema
+from fast_zero.schemas import TodoPublic, TodoSchema, TodoList
 from fast_zero.security import get_current_user
 
 router = APIRouter()
 
 CurrentUser = Annotated[User, Depends(get_current_user)]
 
-router = APIRouter(prefix='/todos', tags=['todos'])
+router = APIRouter(prefix="/todos", tags=["todos"])
 
 
-@router.post(
-    '/', status_code=status.HTTP_201_CREATED, response_model=TodoPublic
-)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=TodoPublic)
 def create_todo(
     todo: TodoSchema,
     user: CurrentUser,
@@ -34,3 +33,29 @@ def create_todo(
     session.refresh(db_todo)
 
     return db_todo
+
+
+@router.get("/", status_code=status.HTTP_200_OK, response_model=TodoList)
+def list_todos(
+    session: Session,
+    user: CurrentUser,
+    title: str = Query(None),
+    description: str = Query(None),
+    state: str = Query(None),
+    offset: int = Query(None),
+    limit: int = Query(None),
+):
+    query = select(Todo).where(Todo.user_id == user.id)
+
+    if title:
+        query = query.filter(Todo.title.contains(title))
+
+    if description:
+        query = query.filter(Todo.description.contains(description))
+
+    if state:
+        query = query.filter(Todo.state == state)
+
+    todos = session.scalars(query.offset(offset).limit(limit)).all()
+
+    return {"todos": todos}
