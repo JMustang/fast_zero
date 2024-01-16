4ad82889293d9ae5ef1f6f88be95436f39d50992
Junior <eecfredes@gmail.com>
2024-01-15 21:33:33 -0300

    :rocket: Criando uma factory para fazer testes da rota todos

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -1,16 +1,31 @@
+import factory.fuzzy
+
+from fast_zero.models import Todo, TodoState, User
+
+
 def test_create_todo(client, token):
     response = client.post(
-        '/todos/',
-        headers={'Authorization': f'Bearer {token}'},
+        "/todos/",
+        headers={"Authorization": f"Bearer {token}"},
         json={
-            'title': 'Test Todo',
-            'description': 'This is a test todo',
-            'state': 'draft',
+            "title": "Test Todo",
+            "description": "This is a test todo",
+            "state": "draft",
         },
     )
     assert response.json() == {
-        'id': 1,
-        'title': 'Test Todo',
-        'description': 'This is a test todo',
-        'state': 'draft',
+        "id": 1,
+        "title": "Test Todo",
+        "description": "This is a test todo",
+        "state": "draft",
     }
+
+
+class TodoFactory(factory.Factory):
+    class Meta:
+        model = Todo
+
+    title = factory.Faker("text")
+    description = factory.Faker("text")
+    state = factory.fuzzy.FuzzyChoice(TodoState)
+    user_id = 1
